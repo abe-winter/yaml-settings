af77b8d27ae167711c15a67152f257057577bbab
Abe Winter <abe@none>
2017-03-25 02:41:49 -0400

    public everything in SettingsNode

--- src/main/java/SettingsNode.java
+++ src/main/java/SettingsNode.java
@@ -4,22 +4,22 @@ import java.util.Map;
 import java.util.ArrayList;
 
 public class SettingsNode {
-    String type; // one of ()
-    String name; // sometimes shown, used for sync
-    String description; // shown below field for text, next to for switch
-    SettingsNode[] children;
-    String[] options;
+    public String type; // one of ()
+    public String name; // sometimes shown, used for sync
+    public String description; // shown below field for text, next to for switch
+    public SettingsNode[] children;
+    public String[] options;
 
-    static SettingsNode[] fromlist(ArrayList<Map> items){
+    public static SettingsNode[] fromlist(ArrayList<Map> items){
         ArrayList<SettingsNode> ret = new ArrayList<SettingsNode>(items.size());
         for (int i=0;i<items.size();++i) ret.add(new SettingsNode(items.get(i)));
         return ret.toArray(new SettingsNode[0]);
     }
 
-    SettingsNode(){}
+    public SettingsNode(){}
 
     // ctor for loading from json
-    SettingsNode(Map map){
+    public SettingsNode(Map map){
         if (map.containsKey("type")) type = (String)map.get("type");
         if (map.containsKey("name")) name = (String)map.get("name");
         if (map.containsKey("description")) description = (String)map.get("description");
